3bfe2be5414bfab3a422ac3da1c0e136fc372790
YooHyeok <yjk7454@naver.com>
2025-07-31 00:25:02 +0900

    feat: 반응형 상태 data 선언 및  텍스트 보간법 Mustache 적용

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -1,7 +1,19 @@
-<script setup>
+<script>
+export default {
+  // data: function() {
+  data() {
+    return {
+      chennel: "유혁스쿨",
+      subscribe: 2677
+    }
+  },
+}
 </script>
-
+<!-- Text Interpolation(보간법) -->
 <template>
+  <h1>채널명 : {{ chennel /* Mustache 문법 */}}</h1>
+  <h2>구독자수 : {{ subscribe }}</h2>
+  <p>{{ `오늘의 행운 숫자 : ` + Math.ceil(Math.random() * 45) }}</p>
 </template>
 
 <style scoped>
