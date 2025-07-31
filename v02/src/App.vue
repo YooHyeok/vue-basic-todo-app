132904689055c457500d0bc64209ae04faa929f2
YooHyeok <yjk7454@naver.com>
2025-08-01 00:05:02 +0900

    feat: `directive` - v-text 예제

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -4,16 +4,24 @@ export default {
   data() {
     return {
       chennel: "유혁스쿨",
-      subscribe: 2677
+      subscribe: 2677,
+      bool: false,
     }
   },
 }
 </script>
-<!-- Text Interpolation(보간법) -->
 <template>
-  <h1>채널명 : {{ chennel /* Mustache 문법 */}}</h1>
+
+  <!-- Text Interpolation(보간법): {{ 상태변수 등 값 }} 형태로 사용 - Mustache문법 이라고도 불린다 -->
+  <h1>채널명 : {{ chennel }}</h1>
   <h2>구독자수 : {{ subscribe }}</h2>
   <p>{{ `오늘의 행운 숫자 : ` + Math.ceil(Math.random() * 45) }}</p>
+
+  <!-- v-text: 보간법과 비슷한 문법으로 텍스트노드 역할을 한다 - 태그 사이에 직접 데이터를 넣을 수 없음 -->
+  <h1 v-text="chennel"></h1>
+  <h1 v-text="subscribe"></h1>
+  <h1 v-text="bool ? 'true' : 'false'"></h1>
+
 </template>
 
 <style scoped>
