774aab09a05b89d0cd2f10affd633472162b209f
YooHyeok <yjk7454@naver.com>
2025-08-03 19:31:50 +0900

    feat: `methods` - methods 속성 예제

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -3,12 +3,14 @@ import DataBind from '@/components/01-DataBind.vue';
 import ConditRender from '@/components/02-ConditRender.vue';
 import LoopRender from '@/components/03-LoopRender.vue';
 import EventBind from '@/components/04-EventBind.vue';
+import Methods from '@/components/05-Methods.vue';
 export default {
   components: {
     DataBind,
     ConditRender,
     LoopRender,
-    EventBind
+    EventBind,
+    Methods,
   },
   // data: function() {
   data() {
@@ -25,6 +27,7 @@ export default {
     <button @click="activeTab = 'ConditRender'">조건부 렌더링</button>
     <button @click="activeTab = 'LoopRender'">반복 렌더링</button>
     <button @click="activeTab = 'EventBind'">이벤트 바인드</button>
+    <button @click="activeTab = 'Methods'">메소드 속성</button>
   </div>
 
   <!-- 구분선 -->
@@ -33,7 +36,7 @@ export default {
   <ConditRender v-else-if="activeTab == 'ConditRender'" />
   <LoopRender v-else-if="activeTab == 'LoopRender'" />
   <EventBind v-else-if="activeTab == 'EventBind'" />
-
+  <Methods v-else-if="activeTab == 'Methods'" />
 </template>
 
 <style scoped>
---
+++ v02/src/components/05-Methods.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data: function() {
+    return {
+      userName: '유혁스쿨',
+      num: 0
+    }
+  },
+  methods: {
+    greeting(userName) {
+      if (userName) return ' Hi, there ' + userName
+      return ' Hi, there ' + this.userName // vue 속성에서 data에 접근할때는 this키워드로 접근한다.
+    },
+    increment() {
+      this.num ++;
+    },
+    decrement() {
+      this.num --;
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <hr/>
+    <a target="_blank" href="https://github.com/YooHyeok/vue-basic-todo-app/blob/main/v02/src/components/05-Methods.vue" style="text-decoration: none; color: blue;">
+      <h1 style="cursor: pointer;">코드 보러 가기</h1>
+    </a>
+    <hr/>
+    <h1>텍스트보간 호출</h1>
+    <h3>{{ greeting() }}</h3>
+    <h3>{{ greeting('코더홀릭') }}</h3>
+    <hr/>
+    
+    <h1>이벤트 바인드</h1>
+    <p>{{ num }}</p>
+    <button v-on:click="increment">Increse Number</button>
+    <button v-on:click="decrement">Decrese Number</button>
+    <hr/>
+  </div>
+</template>
+<style></style>
