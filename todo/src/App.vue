391b28f0336b7db2b2622475d7f3d5d9f43ac582
webdevyoo <yjk7454@naver.com>
2025-08-15 15:02:44 +0900

    feat: 초기 기본 스케폴딩 중 불필요한 컴포넌트 제거 및 기본 main.css 수정

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,47 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="todo-container">
+    <h1>TODO List</h1>
+    <form id="todo-form">
+      <input type="text" id="todo-input" placeholder="Enter a new task" />
+      <button type="button">Add</button>
+    </form>
+    <ul id="todo-list">
+      <li>
+        Sample Task 1
+        <button type="button">Delete</button>
+      </li>
+      <li class="completed">
+        Sample Task 2
+        <button type="button">Delete</button>
+      </li>
+      <li>
+        Sample Task 3
+        <button type="button">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
