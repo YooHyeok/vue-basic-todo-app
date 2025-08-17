a2c2578daf9d5f1399c740f9f3254e7bbc4a0b23
webdevyoo <yjk7454@naver.com>
2025-08-17 20:22:08 +0900

    feat: `v03 - 01)RefReactive` 반응형 변수 Ref, Reactive 예제 및 개념 정리, Ref DOM 조작 예제 추가

--- v03/src/App.vue
+++ v03/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
 import { ref } from 'vue';
 import Counter from './components/00-Counter.vue'
+import RefReactive from './components/01-RefReactive.vue'
 const activeTab = ref('Counter')
 </script>
 <template>
   <!-- 헤더 탭 -->
   <div>
     <button @click="activeTab = 'Counter'">Counter 예제</button>
+    <button @click="activeTab = 'RefReactive'">RefReactive 예제</button>
   </div>
 
   <!-- 구분선 -->
   <hr>
   <Counter v-if="activeTab == 'Counter'" />
+  <RefReactive v-if="activeTab == 'RefReactive'" />
   
 </template>
 
---
+++ v03/src/components/01-RefReactive.vue
@@ -0,0 +1,40 @@
+<script setup>
+/**
+ref, reactive
+반응형 상태 변수로 사용된다.
+
+ref
+타입에 상관없이 사용되나 String, Number Boolean 등 원시타입에 주로 사용된다.  
+Vue2와 동일하게 DOM에 접근할때도 사용된다.
+
+reactive
+Object, Array, Map, Set 같은 참조타입만 사용할 수 있다.  
+ */
+
+import { ref, reactive } from 'vue';
+
+const h1 = ref(null)
+
+const count = ref(0);
+
+const obj1 = reactive({
+  name: 'kim', age: 33
+})
+
+const good = () => {
+  count.value++;
+  obj1.name = 'yoo'
+  obj1.age = 34
+  console.log("h1: ", h1.value)
+  h1.value.innerText += "innerText+=바보 실행됨"
+}
+
+</script>
+
+<template>
+  <h1 ref="h1">Ref DOM 접근</h1>
+  <p>count : {{ count }} </p>
+  <p>obj1 : {{ obj1.name }} / {{ obj1.age }}</p>
+
+  <button @click="good">클릭</button>
+</template>
