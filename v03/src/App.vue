12a168c575f2cb6dd6bc0fe207306a7bda56ccbb
webdevyoo <yjk7454@naver.com>
2025-08-18 01:01:56 +0900

    feat: `v03 - 02)ClassBind` v-bind를 활용한 동적 class 조작 예제 추가

--- v03/src/App.vue
+++ v03/src/App.vue
@@ -1,20 +1,26 @@
 <script setup>
+
 import { ref } from 'vue';
 import Counter from './components/00-Counter.vue'
 import RefReactive from './components/01-RefReactive.vue'
+import ClassBind from './components/02-ClassBind.vue'
+
 const activeTab = ref('Counter')
+
 </script>
 <template>
   <!-- 헤더 탭 -->
   <div>
     <button @click="activeTab = 'Counter'">Counter 예제</button>
     <button @click="activeTab = 'RefReactive'">RefReactive 예제</button>
+    <button @click="activeTab = 'ClassBind'">ClassBind 예제</button>
   </div>
 
   <!-- 구분선 -->
   <hr>
   <Counter v-if="activeTab == 'Counter'" />
   <RefReactive v-if="activeTab == 'RefReactive'" />
+  <ClassBind v-if="activeTab == 'ClassBind'" />
   
 </template>
 
---
+++ v03/src/components/02-ClassBind.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, reactive } from 'vue';
+const titleClass = ref('')
+function good() {
+  titleClass.value = 'title'
+}
+</script>
+
+<template>
+  <a target="_blank" href="https://github.com/YooHyeok/vue-basic-todo-app/blob/main/v03/src/components/02-ClassBind" style="text-decoration: none; color: blue;">
+    <h1 style="cursor: pointer;">코드 보러 가기</h1>
+  </a>
+  <hr/>
+  <h1 :class="titleClass">가능한 빨간색이</h1>
+  <button @click="good">클릭</button>
+</template>
+
+
+<style scoped>
+.title {
+  color: red;
+}
+</style>
