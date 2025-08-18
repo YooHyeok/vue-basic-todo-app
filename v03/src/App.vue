5879f83787a254f5a0d9aba38b7e7f576c9c687d
webdevyoo <yjk7454@naver.com>
2025-08-19 00:44:36 +0900

    feat: `v03 - 05)Watcher` - watchEffect 예제 추가

--- v03/src/App.vue
+++ v03/src/App.vue
@@ -6,6 +6,7 @@ import RefReactive from './components/01-RefReactive.vue'
 import ClassBind from './components/02-ClassBind.vue'
 import Computed from './components/03-Computed.vue'
 import Watch from './components/04-Watch.vue'
+import WatchEffect from './components/05-WatchEffect.vue'
 
 const activeTab = ref('Counter')
 
@@ -18,6 +19,7 @@ const activeTab = ref('Counter')
     <button @click="activeTab = 'ClassBind'">ClassBind 예제</button>
     <button @click="activeTab = 'Computed'">Computed 예제</button>
     <button @click="activeTab = 'Watch'">Watch 예제</button>
+    <button @click="activeTab = 'WatchEffect'">WatchEffect 예제</button>
   </div>
 
   <!-- 구분선 -->
@@ -27,6 +29,7 @@ const activeTab = ref('Counter')
   <ClassBind v-if="activeTab == 'ClassBind'" />
   <Computed v-if="activeTab == 'Computed'" />
   <Watch v-if="activeTab == 'Watch'" />
+  <WatchEffect v-if="activeTab == 'WatchEffect'" />
   
 </template>
 
---
+++ v03/src/components/05-WatchEffect.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+import { ref, reactive, watchEffect } from 'vue';
+
+
+const counter = () => {
+  count.value ++;
+}
+
+const updateName = () => {
+  name.value = name.value == '홍길동' ? '전우치' : '홍길동'
+}
+const updateMeta = () => {
+  meta.value = meta.value == null ? '메롱' : null
+}
+
+const count = ref(1)
+const name = ref('홍길동')
+const meta = ref(null)
+
+/**
+ * 콜백 함수에 언급되는 반응형 변수들이 감시 대상이 된다.
+ */
+watchEffect(() => {
+  console.log(`count는 ${count.value} 이다.`)
+})
+watchEffect(() => {
+  console.log(`이름은 ${name.value} 이다.`)
+})
+watchEffect(() => {
+  console.log(`meta는 ${meta.value} 이다.`)
+})
+watchEffect(() => {
+  console.log(`count는 ${count.value} 이다. 이름은 ${name.value} 이다. meta는 ${meta.value} 이다.`)
+})
+
+</script>
+
+<template>
+  <a target="_blank" href="https://github.com/YooHyeok/vue-basic-todo-app/blob/main/v03/src/components/05-WatchEffect" style="text-decoration: none; color: blue;">
+    <h1 style="cursor: pointer;">코드 보러 가기</h1>
+  </a>
+  <hr/>
+  <h1>Watch Effect</h1>
+  <p>count : {{ count }}</p>
+  <p>name : {{ name }}</p>
+  <p>meta : {{ meta }}</p>
+  <hr>
+  <button @click="counter">클릭</button>
+  <button @click="updateName">이름 변경</button>
+  <button @click="updateMeta">meta 변경</button>
+</template>
