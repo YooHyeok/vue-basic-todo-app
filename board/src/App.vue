d8e4643cf5a6b7e5980c929c04f3a39077637a2e
webdevyoo <yjk7454@naver.com>
2025-08-24 18:06:41 +0900

    feat: `board` - axios then, catch promise 메소드 활용

--- board/src/App.vue
+++ board/src/App.vue
@@ -5,7 +5,13 @@ const inputData = () => {
   axios.post('http://localhost:3000/member', {
       name: "유혁",
       email: "yh@gmail.com"
+    }).then(res => {
+      console.debug("전송완료\n응답상세내용: ", res)
+      console.table(res)
+    }).catch(err => {
+      console.error("서버에 이상이 있습니다\n오류상세내용: ", err)
     })
+    console.log("비동기적 호출")
 }
 </script>
 
