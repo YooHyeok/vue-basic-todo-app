94cf8f3eb00bd82f67fc78558de01c3b0d475796
webdevyoo <yjk7454@naver.com>
2025-08-24 17:13:36 +0900

    feat: `board` - axios, json-server 연동 예제

--- board/src/App.vue
+++ board/src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
+import axios from 'axios'
+
+const inputData = () => {
+  axios.post('http://localhost:3000/member', {
+      name: "유혁",
+      email: "yh@gmail.com"
+    })
+}
 </script>
 
 <template>
-  
+  <button @click="inputData">데이터 입력</button>
 </template>
 
 <style scoped>
