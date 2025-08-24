b2346173ac2b4facab532d0e49393c1403869135
webdevyoo <yjk7454@naver.com>
2025-08-25 00:16:19 +0900

    feat: `main.js - vue 엔트리포인트에 bootstrap 라이브러리 플러그인으로 등록

--- board/src/main.js
+++ board/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {createBootstrap} from 'bootstrap-vue-next/plugins/createBootstrap'
+
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+createApp(App)
+.use(createBootstrap())
+.mount('#app')
