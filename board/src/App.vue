b11011e81bed1a6677ce48ddfcc8c0ab897e6cb8
webdevyoo <yjk7454@naver.com>
2025-08-26 00:15:14 +0900

    refactor: `App.vue` - axios 호출 메소드를 Promise의 await & async 방식으로 적용

--- board/src/App.vue
+++ board/src/App.vue
@@ -4,24 +4,53 @@ import { ref } from 'vue'
 
 const table = ref(null)
 
+/* ===================== propmise then catch ===================== */
 const inputData = () => {
-  axios.post('http://localhost:3000/member', {
-      name: "유혁",
-      email: "yh@gmail.com"
-    }).then(res => {
-      console.debug("전송완료\n응답상세내용: ", res)
-      console.table(res)
-    }).catch(err => {
-      console.error("서버에 이상이 있습니다\n오류상세내용: ", err)
-    })
-    console.log("비동기적 호출")
+  inputDataAxios()
+  .then(res => {
+    console.debug("전송완료\n응답상세내용: ", res)
+    console.table(res)
+  })
+  .catch(err => console.error("서버에 이상이 있습니다\n오류상세내용: ", err))
+  console.log("비동기적 호출")
 }
 
 const getData = () => {
   axios.get('http://localhost:3000/member')
-  .then(res => {
-    const members = res.data
-    let htmlContent = `
+  .then(getDataSuccessCallback)
+  .error(err => console.error(err))
+}
+/* ===================== propmise async await ===================== */
+const inputDataAsync = async () => {
+  try {
+    const res = await inputDataAxios();
+    console.debug("전송완료\n응답상세내용: ", res)
+    console.table(res)
+  } catch(err) {
+    console.error("서버에 이상이 있습니다\n오류상세내용: ", err)
+  }
+  console.log("비동기적 호출")
+}
+
+const getDataAsync = async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/member')
+    getDataSuccessCallback(res);
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+const inputDataAxios = async () => {
+  return await axios.post('http://localhost:3000/member', {
+    name: "유혁",
+    email: "yh@gmail.com"
+  })
+}
+
+const getDataSuccessCallback = (res) => {
+  const members = res.data
+ let htmlContent = `
     <table class="table ms-3 me-3">
       <tr>
         <th>Id</th>
@@ -38,14 +67,13 @@ const getData = () => {
     })
     htmlContent += `</table>`
     table.value.innerHTML = htmlContent;
-  })
 }
 
 </script>
 
 <template>
-  <button @click="inputData" class="btn btn-primary ms-3">데이터 입력</button>
-  <button @click="getData" class="btn btn-success ms-3">데이터 가져오기</button>
+  <button @click="inputDataAsync" class="btn btn-primary ms-3">데이터 입력</button>
+  <button @click="getDataAsync" class="btn btn-success ms-3">데이터 가져오기</button>
   <div ref="table">
 
   </div>
