dc1858c83747df84a41505f7ccf8699fab38a990
webdevyoo <yjk7454@naver.com>
2025-08-28 00:10:53 +0900

    feat: `board` - axios delete json-server 데이터 삭제 구현

--- board/src/App.vue
+++ board/src/App.vue
@@ -3,13 +3,15 @@ import V1AxiosThenCatch from '@/components/V1AxiosThenCatch.vue';
 import V2AxiosAsyncAwait from '@/components/V2AxiosAsyncAwait.vue';
 import V3InputSave from '@/components/V3InputSave.vue';
 import V4VFor from '@/components/V4VFor.vue';
+import V5VDelete from '@/components/V5AxiosDelete.vue';
 </script>
 
 <template>
   <!-- <V1AxiosThenCatch /> -->
   <!-- <V2AxiosAsyncAwait /> -->
   <!-- <V3InputSave /> -->
-  <V4VFor />
+  <!-- <V4VFor /> -->
+  <V5VDelete />
 </template>
 
 <style scoped>
---
+++ board/src/components/V5AxiosDelete.vue
@@ -0,0 +1,93 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const table = ref(null)
+const isInputMode = ref(false)
+const name = ref('')
+const email = ref('')
+const members = ref([])
+
+/* ===================== propmise async await ===================== */
+const inputData = async () => {
+  try {
+    let param = {
+      name: name.value,
+      email: email.value
+    }
+    const res = await axios.post('http://localhost:3000/member', param);
+    console.debug("전송완료\n응답상세내용: ", res)
+    name.value = '';
+    email.value = '';
+    isInputMode.value = false;
+    getData();
+  } catch(err) {
+    console.error("서버에 이상이 있습니다\n오류상세내용: ", err)
+  }
+  console.log("비동기적 호출")
+}
+
+const getData = async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/member')
+    members.value = res.data
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+const deleteData = async (id) => {
+  if (!confirm('삭제 하시겠습니까?')) return
+  try {
+    const res = await axios.delete(`http://localhost:3000/member/${id}`)
+    // await axios.delete(`http://localhost:3000/member`,{data: {id}}) // json-server에서는 req body 사용 불가
+    getData();
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <button @click="isInputMode = true" class="btn btn-primary ms-3">데이터 입력</button>
+    <button @click="getData" class="btn btn-success ms-3">데이터 가져오기</button>
+    <div v-show="isInputMode">
+      <form autocomplete="off">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" v-model="name">
+        <label class="form-label">Email</label>
+        <input type="text" class="form-control" v-model="email">
+        <button type="button" class="btn btn-success" @click="inputData">입력 확인</button>
+        <button type="button" class="btn btn-secondary" @click="isInputMode = false">취소</button>
+      </form>
+    </div>
+    <div ref="table">
+      <table class="table ms-3" v-show="members.length > 0">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="member in members" :key="member.id">
+            <td>{{ member.id }}</td>
+            <td>{{ member.name }}</td>
+            <td>{{ member.email }}</td>
+            <td><button type="button" @click="deleteData(member.id)" class="btn btn-danger">삭제</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  margin-top: 10px
+}
+</style>
