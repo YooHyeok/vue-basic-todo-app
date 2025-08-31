da731e227af7744810135450adbc6dccdd4a41f0
webdevyoo <yjk7454@naver.com>
2025-08-31 19:41:31 +0900

    feat: `vue-router` - route, router 내장객체 script접근 useRoute, useRouter 예제코드

--- vue-router/src/components/HelloView.vue
+++ vue-router/src/components/HelloView.vue
@@ -1,3 +1,18 @@
 <template>
   <p>Hello View</p>
+  <p>{{ route }}</p>
+  <button @click="router.push('about')">hello 페이지로 이동</button>
 </template>
+<!-- 
+[route] 내장객체
+스크립트에서 접근: useRoute
+
+[router] 내장객체
+스크립트에서 접근: useRouter
+ -->
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+</script>
