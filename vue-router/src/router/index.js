b2af5ffd5567ed9fce5db3cad7f15589f8e1cef7
webdevyoo <yjk7454@naver.com>
2025-08-31 19:53:34 +0900

    feat: `vue-router` - 라우트 name 속성 추가

--- vue-router/src/router/index.js
+++ vue-router/src/router/index.js
@@ -6,14 +6,17 @@ import HomeView from "@/components/HomeView";
 const routes = [
   {
     path: '/',
+    name: 'HomePage', /* 주로 path가 /:id 와 같이 동적파라미터(필수)로 되어있을 경우 push를 호출할 때 name을 사용하면 편하다. */
     component: HomeView
   },
   {
     path: '/about',
+    name: 'AboutPage', /* /about/:id일 경우 push('/about/pk') 혹은 push({name:'AboutPage', params{id: 'pk'}}) 형태로 호출 */
     component: AboutView
   },
   {
     path: '/hello',
+    name: 'HelloPage',
     component: HelloView
   },
 ]
