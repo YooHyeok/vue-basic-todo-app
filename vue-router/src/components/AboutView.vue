6557934d07386d824d4aded1b1deb90a6d302122
webdevyoo <yjk7454@naver.com>
2025-08-31 19:41:17 +0900

    feat: `vue-router` - route, router 내장객체 template 접근 예제코드

--- vue-router/src/components/AboutView.vue
+++ vue-router/src/components/AboutView.vue
@@ -1,3 +1,12 @@
 <template>
   <p>About View</p>
+  <p>{{ $route }}</p>
+  <button @click="$router.push('/hello')">hello 페이지로 이동</button>
 </template>
+<!-- 
+[route] 내장객체
+템플릿에서 접근: $route
+
+[router] 내장객체
+템플릿에서 접근: $router
+ -->
