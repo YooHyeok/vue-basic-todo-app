15fa2c8e3b49c5e76f826318af555d9f18b00063
webdevyoo <yjk7454@naver.com>
2025-09-03 00:11:15 +0900

    feat: `vue-router` - CompanyView.vue router-link, router-view 적용

--- vue-router/src/components/company/CompanyView.vue
+++ vue-router/src/components/company/CompanyView.vue
@@ -1,4 +1,12 @@
 <template>
   <h1>Company</h1>
   <p>회사 소개 | 오시는 길 | 회사 연혁</p>
+  <p>
+    <strong>path: </strong>
+    <router-link to="/company/intro" active-class="on">회사 소개</router-link> |
+    <router-link to="/company/map" active-class="on">오시는 길</router-link> |
+    <router-link to="/company/history" active-class="on">회사 연혁</router-link>
+  </p>
+  
+  <router-view></router-view>
 </template>
